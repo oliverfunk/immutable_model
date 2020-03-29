267ca050e1fe3de3b5c811dff5957693a9cacf58
Oliver Funk <oli.funk@gmail.com>
2020-03-29 13:27:19 +0200

    trying stuff out

--- lib/src/v3/immutable_value.dart
+++ lib/src/v3/immutable_value.dart
@@ -2,7 +2,7 @@ typedef E CopyConstructor<E>(E entity);
 typedef E Updater<E>(E entity);
 typedef void Validator<E>(E entity);
 
-class ImmutableEntity<E> {
+class ImmutableValuee<E> {
   final Validator<E> validator;
   final CopyConstructor<E> _copy;
   final E defaultValue;
@@ -17,14 +17,38 @@ class ImmutableEntity<E> {
   E _safeInstance() => _entity ?? defaultValue;
 
   E _safeValidate(E entityToValidate) {
-    if (validator != null) validator(entityToValidate);
+    if (validator != null && entityToValidate != null) validator(entityToValidate);
     return entityToValidate;
   }
 
-  ImmutableEntity(this._copy, [this.defaultValue, this.validator]) {
+  ImmutableValuee(this._copy, [this.defaultValue, this.validator]) {
     _safeValidate(defaultValue);
   }
 
   @override
   String toString() => _safeInstance().toString();
+}
+
+abstract class ImmutableEntity<V> {
+  final V defaultValue;
+  V _value;
+
+  V get value => copy(_safeInstance()); // could even maybe use call()
+
+  void set(V nextValue) => _value = validate(nextValue);
+
+  void reset() => _value = null;
+
+  V _safeInstance() => _value ?? defaultValue;
+
+
+  ImmutableEntity([this.defaultValue]) {
+    validate(defaultValue);
+  }
+  
+  V copy(V current);
+  V validate(V toValidate) => toValidate;
+
+  @override
+  String toString() => _safeInstance().toString();  
 }
--- lib/src/v3/test.dart
+++ lib/src/v3/test.dart
@@ -11,11 +11,29 @@ class Heapy {
   String toString() => "$f $s";
 }
 
+class ImmHeapy extends ImmutableEntity<ImmHeapy> {
+  final String f;
+  final String s;
+
+  ImmHeapy._copy(ImmHeapy ih) : f = ih.f, s = ih.s;
+  ImmHeapy(this.f, this.s);
+
+  @override
+  ImmHeapy copy(ImmHeapy currentValue) => ImmHeapy._copy(currentValue);
+
+  @override
+  ImmHeapy build(ImmHeapy value) => ImmHeapy._copy(value);
+}
+
 void main(){
-  final e = ImmutableEntity<Heapy>(Heapy.copy, Heapy("Oliver", "Funk"));
+  final e = ImmutableValuee<Heapy>(Heapy.copy, Heapy("Oliver", "Funk"));
   final ev1 = e.value;
   e.set((entity) => entity..f = "new");
   final ev2 = e.value;
   print(ev1);
   print(ev2);
+
+  final ih = ImmHeapy("Oliver", "Funk");
+
+  ih.set(nextValue)
 }
